b1a51a9af72ab0a89d4e188818ba4079fdf2c39d
Abilena <abthomas625@gmail.com>
2022-10-24 17:03:57 -0500

    Should have started with this one -_-

---
+++ PrimeFinder/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace PrimeFinder
+{
+    internal class Program
+    {
+        
+    }
+}
---
+++ PrimeFinder/TestPrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PrimeFinder
+{
+    public class TestPrimeNumber
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(3, 5)]
+        [InlineData(10, 29)]
+        [InlineData(13, 41)]
+        [InlineData(25, 97)]
+
+        public void TestAll(int input, int expected)
+        {
+            PrimeNumber pn = new PrimeNumber();
+
+            int actual = pn.GetPrime(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
